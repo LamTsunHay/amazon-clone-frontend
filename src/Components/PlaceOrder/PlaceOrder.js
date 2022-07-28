fe5e621b36681108e8dcc355bb8439be61397988
LamTsunHay <otinilam@gmail.com>
2022-07-29 00:58:14 +0800

    Updates on PlaceOrder left and middle column

--- src/Components/PlaceOrder/PlaceOrder.js
+++ src/Components/PlaceOrder/PlaceOrder.js
@@ -1,16 +1,53 @@
 import React from "react";
-import './PlaceOrder.css'
+import "./PlaceOrder.css";
+import Grid from "@mui/material/Grid";
+import Rating from "@mui/material/Rating";
 
 const PlaceOrder = (props) => {
-    return (
-        <div className="order_container">
-            <div className="order_image_container">
-                Hi
+  return (
+    <Grid container>
+      <Grid item xs={6}>
+        <img
+          className="placeorder_image"
+          src="https://ik.imagekit.io/amazonclone123/amazon-image/mobiles/71w3oJ7aWyL._AC_UL640_FMwebp_QL65__B6qTQl9mia.webp?ik-sdk-version=javascript-1.4.3&updatedAt=1658420308113"
+          alt=""
+        />
+      </Grid>
+      <Grid item xs={4}>
+        <div className="placeorder_description">
+          <div
+            style={{ fontSize: "24px", lineHeight: "32px", fontWeight: 500 }}
+          >
+            New Apple iphone 11 (64GB) - Green
+          </div>
+          <div>
+            <Rating
+              name="read-only"
+              value={3}
+              readOnly
+              style={{ fontSize: "20px" }}
+            />
+            35,990 | 1000+ answered questions
+          </div>
+          <hr />
+          <div>
+            <div className="textgap">
+              Price: <span className="pricetag">HKD 5,999</span>
             </div>
-            <div className="order_description_container">content</div>
-            <div className="order_add_order_container">hu</div>
+            <div className="textgap">
+              FREE delivery: <b>Friday, Jul 29</b>
+            </div>
+            <div className="textgap">
+              EMI starts at HKD 2,401. No Cost EMI avaliable
+            </div>
+          </div>
         </div>
-    );
-}
+      </Grid>
+      <Grid item xs={3}>
+        Order
+      </Grid>
+    </Grid>
+  );
+};
 
 export default PlaceOrder;
