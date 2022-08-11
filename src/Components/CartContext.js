c4cbb6419c61ef4a2e35902e23e0e83769d64fc7
LamTsunHay <otinilam@gmail.com>
2022-08-11 14:03:06 +0800

    Connect to api to addtoCart and fetch product service

--- src/Components/CartContext.js
+++ src/Components/CartContext.js
@@ -3,25 +3,50 @@ import React, { createContext, useState } from "react";
 export const CartContext = createContext();
 
 const CartContextProvider = (props) => {
-  let existingCart =
-    localStorage.getItem("myCart") != undefined
-      ? JSON.parse(localStorage.getItem("myCart"))
-      : [];
+  // let existingCart =
+  //   localStorage.getItem("myCart") != undefined
+  //     ? JSON.parse(localStorage.getItem("myCart"))
+  //     : [];
   const [items, setItems] = useState(existingCart);
   const [size, setSize] = useState(items.length);
 
   const increment = (item) => {
-    let itemList = items;
-    itemList.push(item);
-    setItems(itemList);
-    setSize(itemList.length);
+    let userId = 'user1';
+    let productId = item.id; 
 
-    saveToLocalCache();
+    const payload = {
+      userId: {userId},
+      productId: {productId}
+    }
+
+    const requestOptions = {
+      method: "POST",
+      headers: {'Content-Type': 'application/json'},
+      body: JSON.stringify(payload)
+    }
+    fetch("http://localhost/amazon/addToCart/add", requestOptions)
+
+    updateCart()
+    // let itemList = items;
+    // itemList.push(item);
+    // setItems(itemList);
+    // setSize(itemList.length);
+
+    // saveToLocalCache();
   };
 
-  const saveToLocalCache = () => {
-    localStorage.setItem("myCart", JSON.stringify(items));
-  };
+  const updateCart = () => {
+    fetch("http://localhost/amazon/addToCart/show/user1")
+    .then((response) => response.json())
+    .then((cartList) => {
+      setItems(cartList);
+      setSize(cartList.length);
+    })
+  }
+
+  // const saveToLocalCache = () => {
+  //   localStorage.setItem("myCart", JSON.stringify(items));
+  // };
 
   return (
     <CartContext.Provider value={{ items, size, increment }}>
--- src/Components/DisplayContent/RightSide/RightSidePanel.js
+++ src/Components/DisplayContent/RightSide/RightSidePanel.js
@@ -7,41 +7,43 @@ const RightSidePanel = (props) => {
   const [listOfProduct, setListOfProduct] = useState([]);
 
   useEffect(() => {
-    let list = [
-      {
-        id: "123",
-        name: "New Apple 11 (64GB) - Green",
-        rating: "35,656",
-        price: "5,999",
-        image:
-          "https://ik.imagekit.io/amazonclone123/amazon-image/mobiles/71w3oJ7aWyL._AC_UL640_FMwebp_QL65__B6qTQl9mia.webp?ik-sdk-version=javascript-1.4.3&updatedAt=1658420308113",
-      },
-      {
-        id: "234",
-        name: "New Apple 12 (64GB) - Green",
-        rating: "35,656",
-        price: "5,999",
-        image:
-          "https://ik.imagekit.io/amazonclone123/amazon-image/mobiles/71w3oJ7aWyL._AC_UL640_FMwebp_QL65__B6qTQl9mia.webp?ik-sdk-version=javascript-1.4.3&updatedAt=1658420308113",
-      },
-      {
-        id: "345",
-        name: "New Apple 13 (64GB) - Green",
-        rating: "35,656",
-        price: "5,999",
-        image:
-          "https://ik.imagekit.io/amazonclone123/amazon-image/mobiles/71w3oJ7aWyL._AC_UL640_FMwebp_QL65__B6qTQl9mia.webp?ik-sdk-version=javascript-1.4.3&updatedAt=1658420308113",
-      },
-      {
-        id: "456",
-        name: "New Apple 14 (64GB) - Green",
-        rating: "35,656",
-        price: "6,999",
-        image:
-          "https://ik.imagekit.io/amazonclone123/amazon-image/mobiles/71w3oJ7aWyL._AC_UL640_FMwebp_QL65__B6qTQl9mia.webp?ik-sdk-version=javascript-1.4.3&updatedAt=1658420308113",
-      },
-    ];
-    setListOfProduct(list);
+    fetch("http://localhost:8080/amazon/products/getAllProducts")
+    .then((response) => setListOfProduct(response))
+    // let list = [
+    //   {
+    //     id: "123",
+    //     name: "New Apple 11 (64GB) - Green",
+    //     rating: "35,656",
+    //     price: "5,999",
+    //     image:
+    //       "https://ik.imagekit.io/amazonclone123/amazon-image/mobiles/71w3oJ7aWyL._AC_UL640_FMwebp_QL65__B6qTQl9mia.webp?ik-sdk-version=javascript-1.4.3&updatedAt=1658420308113",
+    //   },
+    //   {
+    //     id: "234",
+    //     name: "New Apple 12 (64GB) - Green",
+    //     rating: "35,656",
+    //     price: "5,999",
+    //     image:
+    //       "https://ik.imagekit.io/amazonclone123/amazon-image/mobiles/71w3oJ7aWyL._AC_UL640_FMwebp_QL65__B6qTQl9mia.webp?ik-sdk-version=javascript-1.4.3&updatedAt=1658420308113",
+    //   },
+    //   {
+    //     id: "345",
+    //     name: "New Apple 13 (64GB) - Green",
+    //     rating: "35,656",
+    //     price: "5,999",
+    //     image:
+    //       "https://ik.imagekit.io/amazonclone123/amazon-image/mobiles/71w3oJ7aWyL._AC_UL640_FMwebp_QL65__B6qTQl9mia.webp?ik-sdk-version=javascript-1.4.3&updatedAt=1658420308113",
+    //   },
+    //   {
+    //     id: "456",
+    //     name: "New Apple 14 (64GB) - Green",
+    //     rating: "35,656",
+    //     price: "6,999",
+    //     image:
+    //       "https://ik.imagekit.io/amazonclone123/amazon-image/mobiles/71w3oJ7aWyL._AC_UL640_FMwebp_QL65__B6qTQl9mia.webp?ik-sdk-version=javascript-1.4.3&updatedAt=1658420308113",
+    //   },
+    // ];
+    // setListOfProduct(list);
   }, []);
 
   return (
